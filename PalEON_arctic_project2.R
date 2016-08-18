ac9d5d27061915c5226840ad0fb47fe4a81ebb1a
istfer <fer.istem@gmail.com>
2016-08-18 18:03:11 -0400

    suface to surface

--- PalEON_arctic_project2.R
+++ PalEON_arctic_project2.R
@@ -4,18 +4,18 @@ library(neotoma)
 
 # get arctic sites pollen datasets
 # surface sample
-arctic_suface_pollen_sites <- get_dataset(datasettype = "pollen surface sample", loc=c(-170, 66, -135, 72))
+arctic_surface_pollen_sites <- get_dataset(datasettype = "pollen surface sample", loc=c(-170, 66, -135, 72))
 # pollen
 arctic_pollen_sites <- get_dataset(datasettype = "pollen", loc=c(-170, 66, -135, 72))
 
-print(arctic_suface_pollen_sites)
+print(arctic_surface_pollen_sites)
 print(arctic_pollen_sites)
 
 # download arctic pollen data
-arctic_suface_pollen_data <- get_download(arctic_suface_pollen_sites)
+arctic_surface_pollen_data <- get_download(arctic_surface_pollen_sites)
 arctic_pollen_data <- get_download(arctic_pollen_sites)
 
-print(arctic_suface_pollen_data)
+print(arctic_surface_pollen_data)
 print(arctic_pollen_data)
 
 #---------------------- read shrub map ----------------------#
@@ -54,7 +54,7 @@ surface_pollen_taxons <- pollen_taxons <- list()
 surface_pollen_counts <- pollen_counts <- list()
 
 # lat - lon - elev - taxon.list - 
-foseq_along(arctic_suface_pollen_data)
+foseq_along(arctic_surface_pollen_data)
 
 arctic_pollen_data[[1]]$dataset$site.data$lat
 
